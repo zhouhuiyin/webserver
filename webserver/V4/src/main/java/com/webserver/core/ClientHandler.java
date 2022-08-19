f6331958d17db64a8f648fbe3290eb93565aa3b5
zhuyahui <albert.zhu@meetsocial.com>
2022-08-19 17:31:25 +0800

    update

--- webserver/V4/src/main/java/com/webserver/core/ClientHandler.java
+++ webserver/V4/src/main/java/com/webserver/core/ClientHandler.java
@@ -3,6 +3,8 @@ package com.webserver.core;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 线程任务
@@ -17,22 +19,8 @@ public class ClientHandler implements Runnable{
     public void run(){
         try {
             //http://localhost:8088
-
             //1.解析请求
-            InputStream in = socket.getInputStream();
-            StringBuilder builder = new StringBuilder();
-            char cur = 'a';//保存本次读取到的字符
-            char pre = 'a';//保存上次读取到的字符
-            int d;
-            while((d = in.read()) !=-1){
-                cur = (char)d;//本次读取到的字符
-                if(pre==13 && cur==10){//判断是否连续读取到了回车+换行
-                    break;
-                }
-                builder.append(cur);
-                pre = cur;//下次循环前将本次读取到的字符记为上次读取的字符
-            }
-            String line = builder.toString().trim();
+            String line = readLine();
             System.out.println("请求行："+line);
             //请求行相关信息
             String method;  //请求方式
@@ -45,9 +33,18 @@ public class ClientHandler implements Runnable{
             System.out.println("method:"+method);
             System.out.println("uri:"+uri);
             System.out.println("protocol:"+protocol);
-
-
-
+            //读取消息头
+            Map<String,String> headers = new HashMap<>();
+            while (true){
+                line = readLine();
+                if(line.isEmpty()){
+                    break;
+                }
+                System.out.println("消息头："+line);
+                data = line.split(":\\s");
+                headers.put(data[0],data[1] );
+            }
+            System.out.println("headers:"+ headers);
             //2.处理请求
             //3.发送响应
         }catch (IOException e){
@@ -60,5 +57,22 @@ public class ClientHandler implements Runnable{
                 e.printStackTrace();
             }
         }
+    }
+
+    private String readLine() throws IOException {
+        InputStream in = socket.getInputStream();
+        StringBuilder builder = new StringBuilder();
+        char cur = 'a';//保存本次读取到的字符
+        char pre = 'a';//保存上次读取到的字符
+        int d;
+        while((d = in.read()) !=-1){
+            cur = (char)d;//本次读取到的字符
+            if(pre==13 && cur==10){//判断是否连续读取到了回车+换行
+                break;
+            }
+            builder.append(cur);
+            pre = cur;//下次循环前将本次读取到的字符记为上次读取的字符
+        }
+        return builder.toString().trim();
     }
 }
---
+++ webserver/V5/src/main/java/com/webserver/core/ClientHandler.java
@@ -0,0 +1,41 @@
+package com.webserver.core;
+
+import com.webserver.http.HttpServletRequest;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 线程任务
+ * 负责与特定的客户端进行http交互
+ */
+public class ClientHandler implements Runnable{
+    private Socket socket;
+    public ClientHandler(Socket socket){
+        this.socket = socket;
+    }
+
+    public void run(){
+        try {
+            //http://localhost:8088
+           //解析请求
+            HttpServletRequest request = new HttpServletRequest(socket);
+            //2.处理请求
+
+            //3.发送响应
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            //响应后断开连接
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
