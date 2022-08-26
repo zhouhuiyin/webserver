762de73bc36163e76fedb1d758bc747db20c8eb0
zhuyahui <albert.zhu@meetsocial.com>
2022-08-26 16:39:28 +0800

    update

--- webserver/V17/src/main/java/com/webserver/controller/UserController.java
+++ webserver/V17/src/main/java/com/webserver/controller/UserController.java
@@ -2,14 +2,27 @@ package com.webserver.controller;
 
 import com.webserver.http.HttpServletRequest;
 import com.webserver.http.HttpServletResponse;
-import jdk.nashorn.internal.codegen.CompilerConstants;
+import com.webserver.vo.User;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 
 /**
  * 处理用户相关的请求操作
  */
 public class UserController {
+    //该File对象表示用于表示所有用户信息保存的目录
+    private static File userDir;
+
+    static {
+        userDir = new File("./webserver/users");
+        if(!userDir.exists()){
+            userDir.mkdirs();
+        }
+    }
+
     /**
      * 处理用户注册操作的请求
      */
@@ -41,9 +54,33 @@ public class UserController {
             用户名不能重复,若是重复用户名则直接响应页面:reg_have_user.html
             页面里居中显示一行字:该用户已存在,请重新注册.
          */
+        File userFile = new File(userDir,username+".obj");
+        if(userFile.exists()){
+            File file = new File("./webserver/webapps/myweb/reg_have_user.html");
+            response.setEntity(file);
+            return;
+        }
 
+        int age = Integer.parseInt(ageStr);
+        //2将该用户信息保存
+        //将当前用户以一个User对象形式序列化到文件中：用户名.obj
+        User user = new User(username,password,nickname,age);
+        try(
+                ObjectOutputStream oos = new ObjectOutputStream(
+                        new FileOutputStream(
+                                userFile
+                        )
+                );
+                ) {
+            oos.writeObject(user);
+            System.out.println("写出完毕！");
 
+            File file = new File("./webserver/webapps/myweb/reg_success.html");
+            response.setEntity(file);
 
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         System.out.println("注册处理完毕！");
 
